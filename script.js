5cef85297eddd3599195edcc95b0749e5dbb4345
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2023-04-26 15:28:11 -0500

    add logic to get user input;2D;2D

--- script.js
+++ script.js
@@ -8,6 +8,24 @@ const userInputEl = document.getElementById("user-input-el")
 const convertBtn = document.getElementById("convert-btn")
 
 convertBtn.addEventListener("click", function () {
-  let mainValue = Number(userInputEl.value) 
-  console.log(typeof mainValue)
+  let mainNum = Number(userInputEl.value) 
+  console.log(typeof mainNum)
 })
+
+function getLength(num) {
+  return {
+    feet: num * 3.281,
+    meter: num / 3.281
+  }
+}
+
+function getVolume(num) {
+  return {
+    liter: num * 3.785,
+    gallon: num / 3.785
+  }
+}
+
+let volume = getVolume(20)
+
+console.log(volume.gallon.toFixed(3))
