714c8eedfe1813f3c0b563d7501712f6334d088b
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2023-04-26 16:01:32 -0500

    working concept

--- script.js
+++ script.js
@@ -7,15 +7,20 @@
 const userInputEl = document.getElementById("user-input-el")
 const convertBtn = document.getElementById("convert-btn")
 
+const lengthCardResultsEl = document.getElementById("length-card-results")
+const volumeCardResultsEl = document.getElementById("volume-card-results")
+const massCardResults = document.getElementById("mass-card-results")
+
 convertBtn.addEventListener("click", function () {
   let mainNum = Number(userInputEl.value) 
-  console.log(typeof mainNum)
+  renderResults(mainNum)
 })
+
 
 function getLength(num) {
   return {
-    feet: num * 3.281,
-    meter: num / 3.281
+    feet: num * 3.2808,
+    meters: num / 3.2808
   }
 }
 
@@ -33,6 +38,27 @@ function getMass(num) {
   }
 }
 
+function renderLengthResults(num) {
+  const length = getLength(num)
+  lengthCardResultsEl.innerHTML = `${num} meters = ${length.feet.toFixed(3)} feet | ${num} feet = ${length.meters.toFixed(3)} meters`
+}
 
+function renderVolumeResults(num) {
+  const volume = getVolume(num)
+  volumeCardResultsEl.innerHTML = `${num} liters = ${volume.gallon.toFixed(3)} gallons | ${num} gallons = ${volume.liter.toFixed(3)} liters`
+}
+
+function renderMassResults(num) {
+  const mass = getMass(num)
+  massCardResults.innerHTML = `${num} kilos = ${mass.pounds.toFixed(3)} pounds | ${num} pounds = ${mass.kilos.toFixed(3)} kilos`
+}
+
+function renderResults(num) {
+  renderLengthResults(num)
+  renderVolumeResults(num)
+  renderMassResults(num)
+}
+
+renderResults(userInputEl.value)
 
 // console.log(mass.kilos.toFixed(3))
