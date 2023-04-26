01b5ea2f924f098dfe3290b9275c9e73bdff8907
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2023-04-26 15:17:35 -0500

    add logic to get user input

--- script.js
+++ script.js
@@ -3,3 +3,11 @@
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
+
+const userInputEl = document.getElementById("user-input-el")
+const convertBtn = document.getElementById("convert-btn")
+
+convertBtn.addEventListener("click", function () {
+  let mainValue = Number(userInputEl.value) 
+  console.log(typeof mainValue)
+})
